b958c381a903e2c1e53914636c04295d524317f8
Vinicius Oliveira <vinilimadeoliveira@hotmail.com>
2022-09-28 13:04:43 -0300

    Desenvolvendo a procedure "criar jogos"

--- src/main/webapp/SQL/Jogos.sql
+++ src/main/webapp/SQL/Jogos.sql
@@ -47,6 +47,10 @@ BEGIN
 		SET @valido = 1
 	END
 END
+
+DECLARE @valido BIT
+EXEC sp_jogoValido 1,2,@valido OUTPUT 
+print @valido
 --======================================= PROCEDURE JOGO VALIDO DIA=======================================--
 CREATE PROCEDURE sp_jogoValidoDia(@timeA INT, @timeB INT,@dt DATE,@valido BIT OUTPUT)
 AS
@@ -139,10 +143,14 @@ AS
 BEGIN
 	DECLARE @valido BIT
 	DECLARE @tentativas INT;
+	DECLARE @qtdDias INT
+	SELECT @qtdDias = COUNT(dataJogo) FROM fn_obterDatasValidas()
+	SET @qtdDias = @qtdDias + 1
 	SET @tentativas = 0
-	WHILE(@tentativas < 10)
+	SET @sucesso = 0
+	WHILE(@tentativas < @qtdDias)
 	BEGIN		
-		EXEC sp_jogoValido 4,2,@valido OUTPUT 
+		EXEC sp_jogoValido @codTimeA,@codTimeB,@valido OUTPUT 
 		IF(@valido = 1)		
 		BEGIN
 			EXEC sp_verificarData @dt,@valido OUTPUT
@@ -169,15 +177,130 @@ BEGIN
 		END
 		ELSE
 		BEGIN
-			SET @sucesso = 0
+			DECLARE @qtd INT
+			SELECT @qtd = COUNT(*) FROM Jogos
+			WHERE 
+				(codigoTimeA =	@codTimeA AND codigoTimeB = @codTimeB)
+				OR
+				(codigoTimeA = @codTimeB AND codigoTimeB = @codTimeA)
+				AND 
+				datajogo = @dt
+			IF(@qtd > 0)
+			BEGIN
+				SET @sucesso = 1
+			END
 		END
 		SET @tentativas = @tentativas + 1
 	END
 END
+
 --=======TESTE==--
 DECLARE @sucesso BIT
 EXEC sp_alocarJogo 1,2,'12/05/2021', @sucesso OUTPUT
 print @sucesso
 
 SELECT * from Jogos
---======================================= PROCEDURE CRIAR PARTIDAS =======================================--
+DELETE FROM Jogos
+--======================================= FUNCTION BUSCAR GRUPO =======================================--
+CREATE FUNCTION fn_buscarGrupo(@num INT) 
+RETURNS @tbGrupo TABLE(grupo CHAR,codTime INT)
+AS
+BEGIN
+	DECLARE @grupo CHAR;
+	IF(@num = 1)
+	BEGIN
+		SET @grupo = 'A'
+	END
+	IF(@num = 2)
+	BEGIN
+		SET @grupo = 'B'
+	END
+	IF(@num = 3)
+	BEGIN
+		SET @grupo = 'C'
+	END
+	IF(@num = 4)
+	BEGIN
+		SET @grupo = 'D'
+	END
+	INSERT INTO @tbGrupo(grupo,codTime)
+		SELECT grupo,codigoTime from Grupos WHERE grupo = @grupo	
+	RETURN
+END
+--teste
+SELECT * from fn_buscarGrupo(4)
+
+--======================================= PROCEDURE CRIAR JOGOS =======================================--
+CREATE PROCEDURE sp_criarJogos(@tb1 INT, @tb2 INT)
+AS
+BEGIN
+	DECLARE @qtdDias INT
+	SELECT @qtdDias = COUNT(dataJogo) FROM fn_obterDatasValidas()
+	DECLARE @i INT
+	DECLARE @counti INT,@countj INT
+	SET ROWCOUNT 0
+	SELECT grupo,codTime INTO #temp1 FROM fn_buscarGrupo(@tb1)
+
+	SET ROWCOUNT 1
+	SET @counti = 0
+	WHILE (@counti < 4)
+	BEGIN
+		SELECT @i = codTime FROM #temp1
+		SET ROWCOUNT 0
+		SET @countj = 0
+		SELECT grupo,codTime INTO #temp2 FROM fn_buscarGrupo(@tb2)
+		WHILE (@countj < 4)
+		BEGIN			
+			DECLARE @j INT
+			SET ROWCOUNT 1
+			SELECT @j = codTime FROM #temp2
+
+			SET ROWCOUNT 0
+				DECLARE @rand INT
+				SET @rand = (RAND()*@qtdDias)+1
+				DECLARE @dt DATE,@res BIT
+				SELECT @dt = dataJogo FROM fn_obterDatasValidas() WHERE id = @rand
+				EXEC sp_alocarJogo @i,@j,@dt,@res OUTPUT
+			DELETE #temp2 WHERE codTime = @j
+			SET ROWCOUNT 1
+			SELECT @j = codTime FROM #temp2
+			SET @countj = @countj + 1			
+		END
+		DROP TABLE #temp2
+		SET ROWCOUNT 0
+		DELETE #temp1 WHERE codTime = @i
+		SET ROWCOUNT 1
+		SET @counti = @counti + 1
+	END
+	SET ROWCOUNT 0
+	DROP TABLE #temp1
+END
+--=========TESTE
+EXEC sp_criarJogos 1,2
+
+--======================================= PROCEDURE CRIAR RODADAS =======================================--
+CREATE PROCEDURE sp_criarRodadas(@sucesso BIT OUTPUT)
+AS
+BEGIN
+	DECLARE @i INT,@j INT
+	DECLARE @qtdGrupos INT
+	SELECT @qtdGrupos = COUNT(DISTINCT grupo)
+	FROM Grupos
+	SET @qtdGrupos = @qtdGrupos + 1
+	SET @i = 1
+	WHILE(@i < @qtdGrupos)
+	BEGIN
+		SET @j = @i+1
+		WHILE(@j < @qtdGrupos)
+		BEGIN
+			PRINT CAST(@i AS CHAR)+ ' VS ' + CAST(@j AS CHAR)
+			EXEC sp_criarJogos @i,@j
+			SET @j = @j + 1
+		END
+		SET @i = @i + 1
+	END
+END
+--=== TESTE
+DECLARE @s BIT
+EXEC sp_criarRodadas @s OUTPUT
+PRINT @s
