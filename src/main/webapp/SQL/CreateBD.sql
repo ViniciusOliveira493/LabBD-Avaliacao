282fc3fb144e7b6ea768fd1ad891ba6c3f608aaa
mkcai <mkcai@10.10.9.4>
2022-09-30 16:13:20 -0300

    CreateBD modificado

--- src/main/webapp/SQL/CreateBD.sql
+++ src/main/webapp/SQL/CreateBD.sql
@@ -5,22 +5,22 @@ GO
 CREATE TABLE Times (codigoTime INT PRIMARY KEY, nomeTime VARCHAR(20), cidade VARCHAR(90), estadio VARCHAR(30))
 GO
 INSERT INTO Times VALUES
-	(1, 'Botafogo-SP', 'Ribeir�o Preto','Santa Cruz'),
-	(2, 'Corinthians','S�o Paulo','Neo Qu�mica Arena'),
-	(3,'Ferrovi�ria','Araraquara','Fonte Luminosa'),
-	(4,'Guarani','Campinas','Brinco de Ouro'),
-	(5,'Inter de Limeira', 'Limeira','Limeir�o'),
-	(6,'Ituano','Itu','Novelli J�nior'),
-	(7,'Mirassol','Mirassol','Jos� Maria de Campos Maia'),
-	(8,'Novorizontino','Novo Horizonte','Jorge Ismael de Biasi'),
-	(9,'Palmeiras','S�o Paulo','Allianz Parque'),
-	(10,'Ponte Preta','Campinas','Mois�s Lucarelli'),
-	(11,'Red Bull Bragantino','Bragan�a Paulista','Nabi Abi Chedid'),
-	(12,'Santo Andr�','Santo Andr�','Bruno Jos� Daniel'),
-	(13,'Santos','Santos','Vila Belmiro'),
-	(14,'S�o Bento','Sorocaba','Walter Ribeiro'),
-	(15,'S�o Caetano','S�o Caetano do Sul','Anacletto Campanella'),
-	(16,'S�o Paulo','S�o Paulo','Morumbi')
+	(1, 'Corinthians','São Paulo','Neo Química Arena'),
+    (2,'Palmeiras','São Paulo','Allianz Parque'),
+	(3,'Santos','Santos','Vila Belmiro'),
+	(4,'São Paulo','São Paulo','Morumbi'),
+	(5, 'Botafogo-SP', 'Ribeirão Preto','Santa Cruz'),
+	(6,'Ferroviária','Araraquara','Fonte Luminosa'),
+	(7,'Guarani','Campinas','Brinco de Ouro'),
+	(8,'Inter de Limeira', 'Limeira','Limeirão'),
+	(9,'Ituano','Itu','Novelli Júnior'),
+	(10,'Mirassol','Mirassol','José Maria de Campos Maia'),
+	(11,'Novorizontino','Novo Horizonte','Jorge Ismael de Biasi'),
+	(12,'Ponte Preta','Campinas','Moisés Lucarelli'),
+	(13,'Red Bull Bragantino','Bragança Paulista','Nabi Abi Chedid'),
+	(14,'Santo André','Santo André','Bruno José Daniel'),
+	(15,'São Bento','Sorocaba','Walter Ribeiro'),
+	(16,'São Caetano','São Caetano do Sul','Anacletto Campanella')
 GO
 --========================== Create table Grupos =============================--
 CREATE TABLE Grupos(grupo CHAR,
