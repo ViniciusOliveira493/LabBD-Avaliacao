984ee738b60cd61fdde0bf5e2b550151ee0e8249
mkcai <mkcai@10.10.9.4>
2022-09-30 16:04:59 -0300

    Outro

--- src/main/webapp/SQL/CreateBD.sql
+++ src/main/webapp/SQL/CreateBD.sql
@@ -5,26 +5,26 @@ GO
 CREATE TABLE Times (codigoTime INT PRIMARY KEY, nomeTime VARCHAR(20), cidade VARCHAR(90), estadio VARCHAR(30))
 GO
 INSERT INTO Times VALUES
-	(1, 'Botafogo-SP', 'Ribeirão Preto','Santa Cruz'),
-	(2, 'Corinthians','São Paulo','Neo Química Arena'),
-	(3,'Ferroviária','Araraquara','Fonte Luminosa'),
+	(1, 'Botafogo-SP', 'Ribeirï¿½o Preto','Santa Cruz'),
+	(2, 'Corinthians','Sï¿½o Paulo','Neo Quï¿½mica Arena'),
+	(3,'Ferroviï¿½ria','Araraquara','Fonte Luminosa'),
 	(4,'Guarani','Campinas','Brinco de Ouro'),
-	(5,'Inter de Limeira', 'Limeira','Limeirão'),
-	(6,'Ituano','Itu','Novelli Júnior'),
-	(7,'Mirassol','Mirassol','José Maria de Campos Maia'),
+	(5,'Inter de Limeira', 'Limeira','Limeirï¿½o'),
+	(6,'Ituano','Itu','Novelli Jï¿½nior'),
+	(7,'Mirassol','Mirassol','Josï¿½ Maria de Campos Maia'),
 	(8,'Novorizontino','Novo Horizonte','Jorge Ismael de Biasi'),
-	(9,'Palmeiras','São Paulo','Allianz Parque'),
-	(10,'Ponte Preta','Campinas','Moisés Lucarelli'),
-	(11,'Red Bull Bragantino','Bragança Paulista','Nabi Abi Chedid'),
-	(12,'Santo André','Santo André','Bruno José Daniel'),
+	(9,'Palmeiras','Sï¿½o Paulo','Allianz Parque'),
+	(10,'Ponte Preta','Campinas','Moisï¿½s Lucarelli'),
+	(11,'Red Bull Bragantino','Braganï¿½a Paulista','Nabi Abi Chedid'),
+	(12,'Santo Andrï¿½','Santo Andrï¿½','Bruno Josï¿½ Daniel'),
 	(13,'Santos','Santos','Vila Belmiro'),
-	(14,'São Bento','Sorocaba','Walter Ribeiro'),
-	(15,'São Caetano','São Caetano do Sul','Anacletto Campanella'),
-	(16,'São Paulo','São Paulo','Morumbi')
+	(14,'Sï¿½o Bento','Sorocaba','Walter Ribeiro'),
+	(15,'Sï¿½o Caetano','Sï¿½o Caetano do Sul','Anacletto Campanella'),
+	(16,'Sï¿½o Paulo','Sï¿½o Paulo','Morumbi')
 GO
 --========================== Create table Grupos =============================--
 CREATE TABLE Grupos(grupo CHAR,
-	codigoTime INT FOREIGN KEY REFERENCES Times(CodigoTime),
+	codigoTime INT FOREIGN KEY REFERENCES Times(CodigoTime) UNIQUE,
 	CONSTRAINT pk_GCT PRIMARY KEY(GRUPO,CodigoTime))
 GO
 --========================== Create table Jogos =============================--
