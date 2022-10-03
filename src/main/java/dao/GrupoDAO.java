981bd59feff8bca196bdd282d615a79ea7eed56d
mkcai <mkcai@10.10.9.4>
2022-10-03 02:13:41 -0300

    GrupoDAO

--- src/main/java/dao/GrupoDAO.java
+++ src/main/java/dao/GrupoDAO.java
@@ -25,8 +25,32 @@ public class GrupoDAO extends DAO<Grupo> {
 
 	@Override
 	public Grupo read(Grupo obj) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Grupo> grupos = new ArrayList<Grupo>();
+		StringBuffer query = new StringBuffer();
+		query.append("SELECT gr.Grupo AS Grupo,tm.CodigoTime AS codigoTime,tm.NomeTime AS nomeTime,tm.Cidade AS cidade"
+				+ ",tm.Estadio as estadio ");
+		query.append("FROM Times tm INNER JOIN Grupos gr ");
+		query.append("ON gr.CodigoTime = tm.CodigoTime ");
+		query.append("ORDER BY gr.Grupo ");
+	
+		PreparedStatement ps = cn.prepareStatement(query.toString());
+	
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			Time t = new Time(); 
+			t.setCodigoTime(rs.getInt("codigoTime"));
+			t.setNomeTime(rs.getString("nomeTime"));
+			t.setCidade(rs.getString("cidade"));
+			t.setEstadio(rs.getString("estadio"));
+			
+			Grupo g = new Grupo();
+			g.setGrupo(rs.getString("Grupo").charAt(0));
+			g.setTime(t);
+			grupos.add(g);
+		}	
+		ps.close();
+		rs.close();
+		return grupos;
 	}
 
 	@Override
@@ -50,6 +74,7 @@ public class GrupoDAO extends DAO<Grupo> {
 		CallableStatement cs = cn.prepareCall(sql);
 		cs.execute();
 		cs.close();	
+		
 		StringBuffer query = new StringBuffer();
 		query.append("SELECT gr.Grupo AS Grupo,tm.CodigoTime AS codigoTime,tm.NomeTime AS nomeTime,tm.Cidade AS cidade"
 				+ ",tm.Estadio as estadio ");
